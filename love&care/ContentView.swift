0ae7c640a7c6851664ba9958f481ea4e187aabd7
mohamedEldeeb13 <mooel744@gmail.com>
2023-12-23 13:42:03 +0200

    Initial Commit

---
+++ love&care/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  love&care
+//
+//  Created by Mohamed Abd Elhakam on 23/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ love&care/love_careApp.swift
@@ -0,0 +1,17 @@
+//
+//  love_careApp.swift
+//  love&care
+//
+//  Created by Mohamed Abd Elhakam on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct love_careApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
